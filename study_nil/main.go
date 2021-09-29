6e2ad858f56a53da658cb5fd4a0513f2bd9b72f4
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-09-30 00:07:19 +0900

    add: samples

--- study_nil/main.go
+++ study_nil/main.go
@@ -13,18 +13,31 @@ type Inf interface {
 func main() {
 	// assign nil to ptr
 	var intptr *int
+	fmt.Println(intptr == nil) // true
 	intptr = nil
 	fmt.Println(intptr) // <nil>
 
+	/* * * * * * * * * * * * *  */
+	/* {_type: *int, data: nil} */
+	/* * * * * * * * * * * * *  */
+	wrapped := interface{}(intptr)
+	fmt.Println(wrapped)        // <nil>
+	fmt.Println(wrapped == nil) // false !!
+
 	// assign nil to func
 	var funcptr F
+	fmt.Println(funcptr) // <nil>
 	funcptr = nil
 	fmt.Println(funcptr) // <nil>
 
 	// assign nil to map
 	m := make(map[int]int)
+	fmt.Println(m == nil) // false
 	m = nil
-	fmt.Println(m) // map[]
+	fmt.Println(m)        // map[]
+	fmt.Println(m == nil) // true
+	// panic: assignment to entry in nil map
+	// m[1] = 5
 
 	/* CANNOT assign nil to array */
 	// cannot use nil (untyped nil value) as [3]int value in assignment
@@ -32,10 +45,13 @@ func main() {
 	// l = nil
 
 	// assign nil to slice
-	l := []int{1, 2, 3}
+	l := []int{}
+	fmt.Println(l == nil) // false
 	l = nil
-	fmt.Println(l) // []
+	fmt.Println(l)        // []
+	fmt.Println(l == nil) // true
 
+	// assing nil to data part of interface. ex: {_type: nil, data: nil <-assigned}
 	var enf Enf
 	enf = nil
 	fmt.Println(enf) // <nil>
@@ -48,7 +64,9 @@ func main() {
 
 	var infptr Enfptr
 	infptr = nil
-	fmt.Println(infptr) // <nil>
+	fmt.Println(infptr)             // <nil>
+	fmt.Println(infptr == nil)      // true
+	fmt.Println(Enfptr(nil) == nil) // true
 
 	// assign nil to chan
 	ch := make(chan<- int)
