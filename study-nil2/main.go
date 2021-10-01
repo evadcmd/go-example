88a6ee6542b42fe454b36fa4758c6a7f4400f592
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-10-01 15:18:39 +0900

    revise some examples

--- study-nil2/main.go
+++ study-nil2/main.go
@@ -3,16 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	// nil slices
+	/* a nil slice */
 	var s []struct{}
-
+	fmt.Println(s)        // []
+	fmt.Println(s == nil) // true
+	// show the len && cap of the slice
 	fmt.Println(len(s)) // 0
 	fmt.Println(cap(s)) // 0
-
 	// iterates zero times
 	for range s {
 	}
 
-	// panic: index out of range
-	fmt.Println(s[1])
+	// panic: runtime error: index out of range [1] with length 0
+	// fmt.Println(s[1])
+
+	/* a nil slice */
+	var m map[int]float64
+	fmt.Println(m)        // map[]
+	fmt.Println(m == nil) // true
+	// show the len of the map
+	fmt.Println(len(m)) // 0
+	// iterates zero times
+	for k, v := range m {
+		fmt.Printf("%v: %v", k, v)
+	}
+	// It is OK to read even the map is nil
+	fmt.Println((m[5])) // 0
+
+	m[5] = 5.2 // panic: assignment to entry in nil map
 }
