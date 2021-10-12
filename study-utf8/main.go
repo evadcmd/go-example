64f4fae19342a260073ffe11fb239b200db711f2
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-10-12 18:38:08 +0900

    [ADD] a utf8 source code study

---
+++ study-utf8/main.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+const (
+	locb = 0b10000000
+	hicb = 0b10111111
+
+	xx = 0xF1 // invalid: size 1
+	as = 0xF0 // ASCII: size 1
+	s1 = 0x02 // accept 0, size 2
+	s2 = 0x13 // accept 1, size 3
+	s3 = 0x03 // accept 0, size 3
+	s4 = 0x23 // accept 2, size 3
+	s5 = 0x34 // accept 3, size 4
+	s6 = 0x04 // accept 0, size 4
+	s7 = 0x44 // accept 4, size 4
+)
+
+var first = [256]uint8{
+	//   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x00-0x0F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x10-0x1F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x20-0x2F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x30-0x3F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x40-0x4F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x50-0x5F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x60-0x6F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x70-0x7F
+	//   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x80-0x8F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x90-0x9F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xA0-0xAF
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xB0-0xBF
+	xx, xx, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xC0-0xCF
+	s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xD0-0xDF
+	s2, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s4, s3, s3, // 0xE0-0xEF
+	s5, s6, s6, s6, s7, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xF0-0xFF
+}
+
+// acceptRange gives the range of valid values for the second byte in a UTF-8
+// sequence.
+type acceptRange struct {
+	lo uint8 // lowest value for second byte.
+	hi uint8 // highest value for second byte.
+}
+
+// acceptRanges has size 16 to avoid bounds checks in the code that uses it.
+var acceptRanges = [16]acceptRange{
+	0: {locb, hicb},
+	1: {0xA0, hicb},
+	2: {locb, 0x9F},
+	3: {0x90, hicb},
+	4: {locb, 0x8F},
+}
+
+func FullRune(p []byte) bool {
+	n := len(p)
+	if n == 0 {
+		return false
+	}
+	x := first[p[0]]
+	if n >= int(x&7) {
+		return true // ASCII, invalid or valid.
+	}
+	// Must be short or invalid.
+	accept := acceptRanges[x>>4]
+	if n > 1 && (p[1] < accept.lo || accept.hi < p[1]) { // 2nd byte is not well formed, so is a error rune
+		return true
+	} else if n > 2 && (p[2] < locb || hicb < p[2]) { // 3rd byte is not well formed, so is a error rune
+		return true
+	}
+	return false
+}
+
+func main() {
+	// EA accept 0, size 3  0b10000000<b1<0b10111111
+	// it is well formed until 2nd byte, but shorter than expectd(not full)
+	// so FullRune() == false
+	sample1 := []byte{0b11101010, 0b10000001}
+	fmt.Printf("%X\n", sample1)
+	fmt.Println(string(sample1))
+	fmt.Println(FullRune(sample1))
+
+	// because it is a 3 bytes word (0b_111_01010),
+	// any byte slice longer than length 3 will be consider a "full rune"
+	// [REASON]
+	// error rune is alse considered as a full rune,
+	// so wether or not it could be decoded sucessfully,
+	// the sample provides enouth length for decoding a rune.
+	sample2 := []byte{0b11101010, 0b10000001, 0b10000001}
+	fmt.Printf("%X\n", sample2)
+	fmt.Println(string(sample2))
+	fmt.Println(FullRune(sample2))
+}
